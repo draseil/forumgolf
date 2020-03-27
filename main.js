c79bb0f16a042978d3b3583b5a9272a3453df377
draseil <lolmart14@gmail.com>
2020-03-27 15:09:56 +0100

    added post array and output all elements in that array

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var http = require('http');
+var url = require('url');
+
+var posts = [
+	["name", "post"],
+	["example_name", "example_post"]
+];
+
+function getPosts(post) {
+	return "name: " + post[0] + "\n" + post[1];
+}
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+	var q = url.parse(req.url, true).query;
+//	res.write(q.thread + '\nHello World!\n');
+	for (var i = 0; i < posts.length; i++) {
+		res.write("name: " + posts[i][0] + "\n" + posts[i][1] + "\n");
+	}
+    res.end();
 }).listen(8080);
