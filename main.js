9bed7c2cf42ef40f39bc532b1a49b52ecf2a5488
draseil <lolmart14@gmail.com>
2020-03-28 10:36:20 +0100

    added POST method and posts array

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 var express = require("express");
+var body_parser = require("body-parser");
 var app = express();
+var urlencoded_parser = body_parser.urlencoded({ extended: false });
+
+var posts = [[]];
 
 app.get("/", function (req, res) {
 	res.setHeader('Content-Type', 'text/html');
-	res.write('<form action="/process_get" method="GET"><label for="username">Username:</label><input type="text" name="username"><br/><label for="post">Post: </label><input type="text" id="post" name="post" required><input type="submit" value="Post!"/></form>');
+	res.write('<form action="/new_post" method="POST"><label for="username">Username:</label><input type="text" name="username"><br/><label for="post">Post: </label><input type="text" id="post" name="post" required><input type="submit" value="Post!"/></form>');
+	for (var i = 0; i < posts.length; i++) {
+		if (posts[i][0] === undefined) {
+			continue;
+		}
+		res.write("User: " + posts[i][0] + "<br/>" + posts[i][1] + "<br/><br/>");
+	}
 	res.end();
 });
 
-app.get("/process_get", function(req, res) {
-	response = {
-		username:req.query.username,
-		post:req.query.post
-	};
-	console.log(response);
-	res.end(JSON.stringify(response));
+app.post("/new_post", urlencoded_parser, function(req, res) {
+	posts.push([req.body.username, req.body.post]);
+	res.end("Post published!  Go back to take a look");
 });
 
 
