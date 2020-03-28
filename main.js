15bb3971ac19c1cc5e00a8aabf480dd5fbaf29ab
draseil <lolmart14@gmail.com>
2020-03-28 22:37:30 +0100

    sanitised input

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ var app = express();
 var urlencoded_parser = body_parser.urlencoded({ extended: false });
 var threads = {"first-thread" : [["", "first-thread"]]};
 var thread_names = Object.keys(threads);
+
+function cleanInput(str) {
+	return str.replace(/&/g, "&amp;").replace(/</g, "&lt;");
+}
+
 app.get("/", function(req, res) {
 	thread_names = Object.keys(threads);
 	for (var i = 0; i < thread_names.length; i++) {
@@ -12,16 +17,23 @@ app.get("/", function(req, res) {
 	res.write('<form action="/new_thread" method="POST"><label for="username">Username:</label><input type="text" name="username"><br/><label for="thread">Thread title: </label><input type="text" name="thread_name" required><input type="submit" value="Create thread!"/></form>');
 	for (var i = 0; i < thread_names.length; i++) {
 		var current_thread = thread_names[i];
+
 		app.post("/" + current_thread + "/new_post", urlencoded_parser, function(req, res) {
-			threads[current_thread].push([req.body.username, req.body.post]);
+			threads[current_thread].push([cleanInput(req.body.username), cleanInput(req.body.post)]);
 			res.end("Post published!  Go back to take a look");
 		});
+
 		app.get("/" + current_thread, function(req, res) {
 			res.setHeader('Content-Type', 'text/html');
 			res.write('<form action="/' + current_thread + '/new_post" method="POST"><label for="username">Username:</label><input type="text" name="username"><br/><label for="post">Post: </label><input type="text" id="post" name="post" required><input type="submit" value="Post!"/></form>');
 			var posts = threads[current_thread];
 			for (var i = 0; i < posts.length; i++) {
-				if (posts[i][0] === undefined) {continue} else if (posts[i][0] === "") {posts[i][0] = "Anonymous"}
+				if (posts[i][0] === undefined) {
+					continue
+				} else if (posts[i][0] === "") {
+					posts[i][0] = "Anonymous"
+				}
+
 				res.write("<b>" + posts[i][0] + "</b><br/>" + posts[i][1] + "<br/><br/>");
 			}
 			res.end();
@@ -29,9 +41,12 @@ app.get("/", function(req, res) {
 	}
 	res.end();
 });
+
 app.post("/new_thread", urlencoded_parser, function(req, res) {
-	threads[req.body.thread_name] = [[req.body.username, req.body.thread_name]];
+	console.log(encodeURIComponent(cleanInput(req.body.thread_name)));
+	threads[encodeURIComponent(cleanInput(req.body.thread_name))] = [[cleanInput(req.body.username), cleanInput(req.body.thread_name)]];
 	res.end("New thread created!  Go back to see it");
 	thread_names = Object.keys(threads);
 });
+
 app.listen(8080);
